b7884491c860105388983a866d6a396eaf0998ef
George Kastrinis <george.kast@gmail.com>
2017-01-05 01:26:44 +0200

    Move HashFunction to a separate file

--- src/main/cpp/HashTable.h
+++ src/main/cpp/HashTable.h
@@ -2,24 +2,7 @@
 
 #include <cstdlib>
 #include <cstdint>
-
-template <typename T>
-class HashFunction {
-public:
-	uint64_t hash(T) { throw 0; }
-};
-
-template <>
-class HashFunction<uint64_t> {
-public:
-	uint64_t hash(uint64_t key) {
-		key = (key ^ (key >> 30)) * UINT64_C(0xbf58476d1ce4e5b9);
-		key = (key ^ (key >> 27)) * UINT64_C(0x94d049bb133111eb);
-		key =  key ^ (key >> 31);
-		return key;
-	}
-};
-
+#include "HashFunction.h"
 
 template <typename T>
 class HashTable {
